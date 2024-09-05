4e75872c3f029cd0f7b99e1c1307c24dfcf6b6b7
voidname <voidpurplesky@gmail.com>
2024-09-05 10:31:13 +0900

    spring boot 설정하고 실행

--- src/main/java/com/example/untitled/UntitledApplication.java
+++ src/main/java/com/example/untitled/UntitledApplication.java
@@ -1,4 +1,10 @@
 package com.example.untitled;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class UntitledApplication {
+
+    public static void main(String[] args) { SpringApplication.run(UntitledApplication.class, args); }
 }
